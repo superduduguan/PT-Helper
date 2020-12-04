e597661ed7c80f210459ade3da9d649166c7edad
superduduguan <superduduguan@gmail.com>
2020-12-04 22:12:03 +0800

    12.4

--- background.js
+++ background.js
@@ -1,14 +1,38 @@
-alert("as");
+site = "http://nanyangpt.com/torrents.php?search=\nhttp://npupt.com/torrents.php?search=\nhttps://bt.byr.cn/torrents.php?search=\nhttps://pt.m-team.cc/torrents.php?search=\n";
+sites = site.split('\n')
+
 chrome.extension.onConnect.addListener(function(port) {
+    //connecting
     console.log("Connected");
+    // port.postMessage(sites)
+
+    //listen for message
     port.onMessage.addListener(function(msg) {
         console.log("message recieved " + msg);
-        chrome.tabs.create({url:msg});
+        for(site in sites){
+            if(sites[site]){
+                chrome.tabs.create({url: sites[site] + msg});
+            }
+        }
     });
 });
 
-
+var index = new Array();
 chrome.contextMenus.create({
-	title: "search this keyword in PT sites",
-	onclick: function(){alert('您点击了右键菜单！');}
+	title: 'Search on PT sites：%s', // %s表示选中的文字
+	contexts: ['selection','link'], // 只有当选中文字时才会出现此右键菜单
+	onclick: function(params)
+	{
+        for(site in sites){
+            if(sites[site]){
+                if(site == 0){
+                    chrome.tabs.create({url: sites[site] + encodeURI(params.selectionText)});
+                }
+                else{
+                    chrome.tabs.create({url: sites[site] + encodeURI(params.selectionText), active:false});
+                }
+                
+            }
+        }
+    }
 });
--- popup.js
+++ popup.js
@@ -1,29 +1,34 @@
-site = "http://nanyangpt.com/torrents.php?search=\nhttp://npupt.com/torrents.php?search=\nhttps://bt.byr.cn/torrents.php?search=\nhttps://pt.m-team.cc/torrents.php?search=\n";
-sites = site.split('\n')
-console.log(sites)
+//global params
+var sites;
 
+//port
 var port = chrome.extension.connect({
     name: "Sample Communication"
 });
 
+//listen
+// port.onMessage.addListener(function(msg) {
+//     console.log("message recieved" + msg);
+//     sites = msg;
+// });
 
+//functional
 function concatInput(sites){
     var input=document.getElementById("txt").value;
-    for(site in sites){
-        if(sites[site]){
-            port.postMessage(sites[site] + input); 
-        }
-    }
+            port.postMessage(input); 
 }
+
 
 window.onload = function() 
 {
+    //listen for click
     var button = document.getElementById('button')
     button.addEventListener('click', function()
     {
         concatInput(sites); 
     }, false);
 
+    //listen for enter
     var input = document.getElementById("txt");
     input.addEventListener("keyup", function(event) {
         if (event.keyCode === 13) {
@@ -35,5 +40,7 @@ window.onload = function()
 
 
 //TODO: 挖掘与聚合
-//TODO: 右键划词
+//TODO: 划词 + 快捷键
 //TODO: 豆瓣跳转
+//TODO: Top4 展示
+//TODO: 转种子
