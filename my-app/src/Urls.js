bf7221a9ace69876fe243e75db9c56cbc403377c
superduduguan <superduduguan@gmail.com>
2021-07-05 04:29:37 +0800

    state lift to both ok + input disable ok

--- my-app/src/Urls.js
+++ my-app/src/Urls.js
@@ -17,6 +17,7 @@ class Urls extends React.Component{
             ckb: [],
             inputval1: '',
 			inputval2: '',
+            over: false
         };
 		this.input1ref = React.createRef();
 		this.input2ref = React.createRef();
@@ -38,29 +39,30 @@ class Urls extends React.Component{
 
     buttonclick1(){
 		if(this.state.inputval1 === ''){
-
 			this.fire("ERROR", "Empty input.", "error");
 			return
 		}
-		if(this.props.name.length === 6){
+		if(this.props.all.name.length === 6){
 			this.fire("ERROR", "Up to 6 websites are accepted.", "error");
 			return
 		}
-		if(this.props.name.length > this.props.add.length ){
+		if(this.props.all.name.length > this.props.all.add.length ){
 			this.fire("ERROR", "Please confirm the last address first.", "error");
 			return
 		}
-		let newname = [...this.props.name, this.state.inputval1];
-		let newckb = [...this.props.ckb, true];
+		let newname = [...this.props.all.name, this.state.inputval1];
+		let newckb = [...this.props.all.ckb, true];
         console.log(newname);
-		this.props.tellname(newname);
-        this.props.tellckb(newckb);
+        this.props.all.ckb = newckb;
+        this.props.all.name = newname;
+		this.props.tellall(this.props.all)
         this.setState((state, props) => ({
-            name: [...props.name, state.inputval1]
+            name: [...props.all.name, state.inputval1]
           }));
         this.setState((state, props) => ({
-            ckb: [...props.ckb, true]
+            ckb: [...props.all.ckb, true]
           }));
+        this.setState({over: true});
     }
 	
     buttonclick2(){
@@ -68,24 +70,21 @@ class Urls extends React.Component{
 			this.fire("ERROR", "Empty input.", "error");
 			return
 		}
-		if(this.props.add.length === 6){
+		if(this.props.all.add.length === 6){
 			this.fire("ERROR", "Up to 6 websites are accepted.", "error");
 			return
 		}
 
-        let newadd = this.props.add
-        let newinput = this.state.inputval2
-        newadd.push(newinput);
-		if(newadd.length > this.props.name.length){
+
+		if(this.props.all.add.length === this.props.all.name.length){
 			this.fire("ERROR", "Please confirm the name first.", "error");
 			return
 		}
 				
-        
-        
-        this.props.telladd(newadd);
-        this.setState({add: newadd});
-		if(this.props.add.length - this.props.name.length === 0){
+        this.props.all.add = [...this.props.all.add, this.state.inputval2]
+        this.props.tellall(this.props.all);
+        this.setState({add: [...this.props.all.add, this.state.inputval2]});
+		if(this.props.all.add.length - this.props.all.name.length === 0){
 			
 			this.input1ref.current.value=' ';
 			this.setState({inputval1: ''});
@@ -94,13 +93,14 @@ class Urls extends React.Component{
 		}
 
 
-		
+		this.setState({over: false});
     }
 	
 	ckb(index, e){
-		let newckb = this.props.ckb
+		let newckb = this.props.all.ckb
 		newckb[index] = e.target.checked
-		this.props.tellckb(newckb);
+        this.props.all.ckb = newckb;
+		this.props.tellall(this.props.all);
         this.setState({ckb: newckb});
 	}
 	
@@ -108,16 +108,17 @@ class Urls extends React.Component{
 	
 	dlt(index, e){
 		
-		const newname = this.props.name;
-		const newadd = this.props.add;
-		const newckb = this.props.ckb;
+		const newname = this.props.all.name;
+		const newadd = this.props.all.add;
+		const newckb = this.props.all.ckb;
 		newname.splice(index, 1);
 		newadd.splice(index, 1);
 		newckb.splice(index, 1);
 		
-		this.props.tellname(newname);
-		this.props.telladd(newadd);
-		this.props.tellckb(newckb);
+		this.props.all.name = newname;
+		this.props.all.add = newadd;
+		this.props.all.ckb = newckb;
+        this.props.tellall(this.props.all);
         this.setState({ckb: newckb});
         this.setState({add: newadd});
         this.setState({name: newname});
@@ -128,7 +129,7 @@ class Urls extends React.Component{
             <div>
                 <div style={{display: 'inline', width:'30%'}}>
 					<h3 style={{display: 'inline', fontSize:'1.75em'}}>Name:    </h3>
-                    <input value={this.state.inputval1} onChange={this.inputchange1.bind(this)} style={{width:'30%', fontSize:'1.75em'}} id='input1' ref={this.input1ref} />
+                    <input value={this.state.inputval1} onChange={this.inputchange1.bind(this)} style={{width:'30%', fontSize:'1.75em'}} disabled = {(this.state.over)? "disabled" : ""} id='input1' ref={this.input1ref} />
                     <button onClick={this.buttonclick1.bind(this)} style={{width:'2.5em', fontSize:'1.75em'}}>add</button>
                 </div>
 				
@@ -139,14 +140,14 @@ class Urls extends React.Component{
 				</div>
 				<div style={{marginLeft: '0'}}>
 					{
-						this.props.name.map(
+						this.props.all.name.map(
 							(item, index)=>
 							{
 								return (
 								<div style={{marginTop: '2em', marginLeft: '20%', fontSize: '150%'}} key={-index-1000}>
 									<img src="./Delete.png" height="20px" onClick={this.dlt.bind(this, index)} alt='delete'></img><span>    </span>
-									<input type="checkbox" key={index} value={item} style={{zoom:'150%'}} onChange={this.ckb.bind(this, index)} checked={this.props.ckb[index]}/>
-									<span>    </span><span style={this.props.ckb[index]? {border: 'cyan solid', borderRadius: '40px', background:'#E0FFFF', padding:'0.4em'}: {padding:'0.4em', borderRadius: '40px'}} key={-index-1}>{item}</span><span style={this.props.ckb[index]? {color:'cyan', marginLeft:'4px', fontWeight:'1000'}: {}}>     =>   </span><span style={this.props.ckb[index]? {border: 'cyan solid', borderRadius: '40px', background:'#E0FFFF', padding:'0.4em'}: {borderRadius: '40px', padding:'0.4em'}} key={-index-100}>{this.props.add[index]}</span>
+									<input type="checkbox" key={index} value={item} style={{zoom:'150%'}} onChange={this.ckb.bind(this, index)} checked={this.props.all.ckb[index]}/>
+									<span>    </span><span style={this.props.all.ckb[index]? {border: 'cyan solid', borderRadius: '40px', background:'#E0FFFF', padding:'0.4em'}: {padding:'0.4em', borderRadius: '40px'}} key={-index-1}>{item}</span><span style={this.props.all.ckb[index]? {color:'cyan', marginLeft:'4px', fontWeight:'1000'}: {}}>     =>   </span><span style={this.props.all.ckb[index]? {border: 'cyan solid', borderRadius: '40px', background:'#E0FFFF', padding:'0.4em'}: {borderRadius: '40px', padding:'0.4em'}} key={-index-100}>{this.props.all.add[index]}</span>
 								</div>
 								)
 								
--- my-app/src/both.js
+++ my-app/src/both.js
@@ -8,7 +8,7 @@ var name = ['NanyangPT', 'NpuPT ', 'ByrPT'];
 var add = ['http://nanyangpt.com/', 'http://npupt.com/', 'https://bt.byr.cn/'];
 var ckb = [true, true, true];
 var opt = [true, true, true, true, true];
-
+var all = {name: name, add: add, ckb: ckb}
 class Both extends React.Component{
 
     constructor(props) {
@@ -18,20 +18,13 @@ class Both extends React.Component{
 	
 	// synchronous states
 	
-	tellname(newname){
+	tellall(all){
 
-        name = [...newname];
-        console.log(name);
+        all = all;
+        console.log(all);
 	}
 
-	telladd(newaddress){
-        add = newaddress;
-        console.log(add);
-	}
-	tellckb(newckb){
-        ckb = newckb;
-        console.log(ckb);
-	}
+
 
 	tellopts(newoptions){
         opt = newoptions; 
@@ -44,7 +37,7 @@ class Both extends React.Component{
 		<div>
 			<div id='url'>
 				<div id='urltop' align="center"><h1>PT Websites</h1></div>
-				<div id="root"><Urls tellname={this.tellname.bind(this)} telladd={this.telladd.bind(this)} tellckb={this.tellckb.bind(this)} add={add} name={name} ckb={ckb} /></div>
+				<div id="root"><Urls tellall={this.tellall.bind(this)} all={all} /></div>
 			</div>
 			<div id='options'>
 				<div id='opttop' align="center"><h1>Helper Options</h1></div>
