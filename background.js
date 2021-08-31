2bbb06a4d54ca8ada98654c7197a5760bc5d4d6b
superduduguan <superduduguan@gmail.com>
2021-08-31 09:57:52 +0800

    headeradded

--- background.js
+++ background.js
@@ -37,13 +37,24 @@ chrome.extension.onConnect.addListener(function(port){
 chrome.runtime.onMessage.addListener(function(request, sender)
 {
 	console.log('收到来自content-script的消息：');
-    console.log(request, sender);
+    // console.log(request, sender);
     if(request.site == 'doubansub'){
         var name = sender.tab.title.slice(start=0, end=-4);
         var id = sender.tab.url.split('/')[4]
-        console.log(name);
-        console.log(id);
-        $.post("https://movie.douban.com/j/subject/"+id+"/interest",{ck:'No3N',interest:'wish',foldcollect:'U'},function(data,status){console.log(status);})
+        console.log('name:', name);
+        console.log('id:', id);
+
+        $.ajax(
+            {
+                url:"https://movie.douban.com/j/subject/"+id+"/interest",
+                type:'post',
+                dateType:'json',
+                headers:{'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'},
+                data:{ck:'8OhY',interest:'wish',foldcollect:'U'},
+                success:function(data){console.log("sucess");},
+                error:function(data){console.log("error");}
+            }
+        );
         chrome.tabs.query({
             active: true,
             currentWindow: true
@@ -53,7 +64,7 @@ chrome.runtime.onMessage.addListener(function(request, sender)
             for(site in sites){
                 if(sites[site]){
                     var newindex = parseInt(tabindex) + parseInt(site) + 1;
-                    console.log(newindex);
+                    // console.log(newindex);
                     if(site == 0){
                         chrome.tabs.create({url: sites[site] + name, index:newindex});
                     }
